b7189ad7f638fc8e0edb50326081a362cea5907f
HansHammel <mail@specialsolutions.de>
2017-11-15 01:52:24 +0100

    Travis releated

--- Drafts/FileUpload2Test/Properties/AssemblyInfo.cs
+++ Drafts/FileUpload2Test/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using Xunit;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("")]
+//[assembly: AssemblyDescription("")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
